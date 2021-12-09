16a3b3f7a4788999ca18ac3336af984619edd876
hasan <hsnkale2021@gmail.com>
2021-12-09 22:56:58 +0300

    1.commit

--- src/test/java/git_github.java
+++ src/test/java/git_github.java
@@ -3,5 +3,9 @@ public class git_github {
 
     public static void main(String[] args) {
         System.out.println("start project");
+
+        System.out.println("1. local master");
+
+        System.out.println("2. local master");
     }
 }
