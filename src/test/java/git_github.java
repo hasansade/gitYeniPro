3c924cbb850be1882ce3223e96f8abdfc86ee2a0
hasan <hsnkale2021@gmail.com>
2021-12-09 23:00:47 +0300

    2. branc commit

--- src/test/java/git_github.java
+++ src/test/java/git_github.java
@@ -3,5 +3,8 @@ public class git_github {
 
     public static void main(String[] args) {
         System.out.println("start project");
+
+        System.out.println("2. branc ");
+        System.out.println("3. branc ");
     }
 }
